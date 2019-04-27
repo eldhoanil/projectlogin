224f27f1baf23c672d7c0a44e536e611b484e155
eldhoanil <42098747+eldhoanil@users.noreply.github.com>
2019-04-27 20:26:43 +0530

    Add files via upload

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,42 +1,96 @@
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended:false});
 var MongoClient = require('mongodb').MongoClient;
+var assert =require('assert');
 var url = 'mongodb://localhost:27017/mydb';
+var urll = 'mongodb://localhost:27017/property';
+var url2 = 'mongodb://localhost:27017/sell';
+var url3 ='mongodb://localhost:27017/AdharDatabase';
 
-var urll = 'mongodb://localhost:27017/deed';
+//var nurl = 'mongodb://localhost:27017/notary';
+var fs= require("fs");
+
 module.exports = (function(app){
 
   app.get('/', function(req,res){
-    res.render('home');
+    res.render('home1');
+  });
+ app.get('/changep',function(req,res){
+    res.render('changep');
+  });
+  app.get('/profile',function(req,res){
+    res.render('profile');
+  });
+  app.get('/view1',function(req,res){
+    res.render('view1');
   });
 
-  app.get('/register',function(req,res){
-    res.render('register');
-  });
-  app.get('/sales1',function(req,res){
-    res.render('sales1');
-  });
   app.get('/login',function(req,res){
     res.render('login');
-  });
+  }); 
+  app.get('/notlog',function(req,res){
+    res.render('notlog');
+  }); 
+  app.get('/notprofile',function(req,res){
+    res.render('notprofile');
+  }); 
+    app.get('/buyer',function(req,res){
+    res.render('buyer');
+  }); 
+    app.get('/pay',function(req,res){
+    res.render('pay');
+  }); 
 
+//notary page=====================================================================
+app.get('/nota',function(req,res){
+ res.render('notary');
+});
 
-
+//land database display=================================================================================
 app.post('/demo1',urlencodedParser,function(req,res){
+ 
    MongoClient.connect(urll, function(err, db) {
-   db.collection('sales').findOne({ Land_id: req.body.Land_id}, function(err, user) {
-             if(user ===null){
-               res.end(" invalid land id");
-            }else if (user.Land_id === req.body.Land_id && user.name === req.body.name ){
-            res.render('completeprofile1',{profileData:user});
+    db.collection('own_property').findOne({ Aadhaarno: req.body.Aadhaarno}, function(err, user) {
 
-          } else {
+              
+             if(user ===null)
+             {
+               res.end(" invalid Aadhaarno");
+            }
+            else if (user.Aadhaarno === req.body.Aadhaarno )
+          { 
+          //  global.owneraddress=user.ownedaddress;
+           var resultArray=[];
+          
+           var cursor=db.collection('own_property').find({ Aadhaarno: req.body.Aadhaarno});
+            cursor.forEach(function(doc,err){
+            assert.equal(null,err);
+            resultArray.push(doc);
+              },function() {
+                db.close();
+
+              res.render('completeprofile1',{profileData:resultArray});
+            });
+           }
+           else {
             console.log("Credentials wrong");
             res.end(" invalid");
           }
-   });
+             
  });
+  });
+ });
+// changeprice TO DB==================================================================
+  app.post('/changeprice',urlencodedParser,function(req,res){
+
+    var price1=req.body.price;
+    var landid=req.body.landid;
+    console.log(price1);
+    console.log(landid);
+    res.send("price has changed");
+   
 });
+
 // Login TO DB==================================================================
   app.post('/demo',urlencodedParser,function(req,res){
    MongoClient.connect(url, function(err, db) {
@@ -44,6 +98,9 @@ app.post('/demo1',urlencodedParser,function(req,res){
              if(user ===null){
                res.end("Login invalid");
             }else if (user.name === req.body.name && user.pass === req.body.pass){
+
+              global.ousername=req.body.name;
+              global.userAadhaarno=user.aadhaar
             res.render('completeprofile',{profileData:user});
 
           } else {
@@ -54,26 +111,309 @@ app.post('/demo1',urlencodedParser,function(req,res){
  });
 });
 
-//register to DB================================================================
-app.post('/regiterToDb',urlencodedParser,function(req,res){
- var obj = JSON.stringify(req.body);
- var jsonObj = JSON.parse(obj);
+//public page=====================================================================
+app.get('/public', function(req,res,next){
+  var resultArray=[];
+  MongoClient.connect(url2,function(err,db){
+    assert.equal(null,err);
+    var cursor=db.collection('propsell').find();
+    cursor.forEach(function(doc,err){
+      assert.equal(null,err);
+      resultArray.push(doc);
+    },function() {
+      db.close();
+      res.render('public1',{questions:resultArray});
+    });
+  });
+});
 
-     res.render('profile',{loginData:req.body});
+//already approved page=====================================================================
+app.get('/already', function(req,res,next){
+  var resultArray=[];
+
+  MongoClient.connect(url2,function(err,db){
+    assert.equal(null,err);
+    var cursor=db.collection('propsell').find({approved:'true'});
+    cursor.forEach(function(doc,err){
+      assert.equal(null,err);
+      resultArray.push(doc);
+    },function() {
+      db.close();
+      res.render('approved',{questions:resultArray});
+    });
+  });
+});
+
+//yet to approve page=====================================================================
+app.get('/yet', function(req,res,next){
+  var resultArray=[];
+  MongoClient.connect(url2,function(err,db){
+    assert.equal(null,err);
+    var cursor=db.collection('propsell').find({approved:'false'});
+    cursor.forEach(function(doc,err){
+      assert.equal(null,err);
+      resultArray.push(doc);
+    },function() {
+      db.close();
+      res.render('yetapproved',{questions:resultArray});
+    });
+  });
+});
+
+
+// Not Login TO DB==================================================================
+  app.post('/notaryy',urlencodedParser,function(req,res){
+   MongoClient.connect('mongodb://localhost:27017/notary', function(err, db) {
+   db.collection('notuserprofile').findOne({ username: req.body.name}, function(err, user) {
+             if(user ===null){
+               res.end("Login invalid");
+            }else if (user.username === req.body.name && user.password === req.body.pass){
+              
+            res.render('notprofile',{profileData:user});
+
+          } else {
+            console.log("Credentials wrong");
+            res.end("Login invalid");
+          }
+   });
+ });
+});
+
+
+//payprice===========================================================================
+app.get('/opay',urlencodedParser,function(req,res){
+          console.log(selleraadhar);
+          
+          console.log(sellerlandid);
+
+
+          MongoClient.connect(url, function(err, db) {
+             db.collection('userprofile').findOne({ aadhaar:selleraadhar}, function(err, user) {
+              global.sellerusername=user.name;
+              console.log(sellerusername);
+            });
+             db.close();
+          });
+          MongoClient.connect('mongodb://localhost:27017/property', function(err, db) {
+             db.collection('own_property').findOne({Aadhaarno:userAadhaarno }, function(err, user) {
+              global.owneraddress=user.ownedaddress;
+              console.log(owneraddress);
+            });
+             db.close();
+          });
+         MongoClient.connect('mongodb://localhost:27017/property', function(err, db) {
+             
+
+              db.collection('own_property').findOne({ landid:sellerlandid},function(err, seller) {
+                if(err) {throw err;
+                  res.send("no such land");
+                }
+                console.log(seller);
+              var myquery = { Aadhaarno:seller.Aadhaarno,ownedaddress:seller.ownedaddress,prevownname:seller.prevownname };
+            var newvalues = { landid:seller.landid,Aadhaarno:userAadhaarno,ownedaddress:owneraddress,prevownname:sellerusername,cordone:seller.cordone,cordtwo:seller.cordtwo,area:seller.area,taxstat:seller.taxstat,liabstat:seller.liabstat,price:seller.price,buyername:"",approved:'false'};
+            db.collection("own_property").updateOne(myquery, newvalues, function(err, res) {
+              if (err) throw err;
+              console.log("1 document updated");
+              db.close();
+            });
+            
+          });
+              });
+        MongoClient.connect(url2, function(err, db) {
+             db.collection('propsell').remove({landid:sellerlandid}, function(err, results) {
+
+            console.log(results.result);
+        });
+        res.send("ownership is changed");
+        db.close();
+          });
+
+   
+ });
+
+//userpage=====================================================================
+app.get('/userr',urlencodedParser,function(req,res){
+
+     res.render('home');
 
   });
-//register profile to MongoDB================================================================
-  app.post('/completeprofile',urlencodedParser,function(req,res){
-   var obj = JSON.stringify(req.body);
-   console.log("Final reg Data : "+obj);
-   var jsonObj = JSON.parse(obj);
-      MongoClient.connect(url, function(err, db) {
-      db.collection("userprofile").insertOne(jsonObj, function(err, res) {
+//register to DB================================================================
+app.get('/regiterToDb',urlencodedParser,function(req,res){
+ 
+     res.render('profile');
+
+  });
+
+//paycheck function============================================================================
+app.post('/paycheck',urlencodedParser,function(req,res){
+
+   MongoClient.connect('mongodb://localhost:27017/sell', function(err, db) {
+   
+
+    db.collection('propsell').findOne({ buyername:ousername},function(err, seller) {
+     
+      
+           if(seller===null){
+            res.send(" there is no property to pay for you");
+            }
+            else if (seller.buyername === ousername)
+            {
+              if(seller.approved === "false")
+              {
+                res.send("notary is not approved your property");
+              }
+              else
+              { global.selleraadhar=seller.Aadhaarno;
+                global.sellerlandid=seller.landid;
+                res.render('paypage',{profileData:seller});
+              }
+            }
+            else{
+               
+              res.end("you are not the buyer");
+            }
+
+        db.close();
+    });
+});
+    });
+
+//cancel btn working===========================================================================
+app.post('/cancel',urlencodedParser,function(req,res){
+   // fs.readFile('test.txt', 'utf8', function(err, data) {  
+   //  if (err) throw err;
+   //  console.log(data);
+   MongoClient.connect('mongodb://localhost:27017/sell', function(err, db) {
+   
+
+    db.collection('propsell').findOne({ landid:req.body.landidd},function(err, user) {
+      console.log(user)
+           if(user === null){
+               res.send("your land is not for sale now");
+            }else {
+              db.collection('propsell').remove({landid:user.landid}, function(err, results) {
+
+            console.log(results.result);
+        });
+        res.send("your land is removed from sale");
+        db.close();
+    }
+});
+    });
+    // });
+ });
+
+app.post('/addb',urlencodedParser,function(req,res){
+ 
+   MongoClient.connect(url2, function(err, db) {
+    db.collection('propsell').findOne({ landid: req.body.landidd}, function(err, user) {
+    
+              
+            //  if(user ===null)
+            //  {
+            //    res.end(" invalid landid");
+            // }
+            if (err) throw err;
+  var myquery = { buyername: "" };
+  var newvalues = { landid:user.landid,Aadhaarno:user.Aadhaarno,ownedaddress:user.ownedaddress,prevownname:user.prevownname,cordone:user.cordone,cordtwo:user.cordtwo,area:user.area,taxstat:user.taxstat,liabstat:user.liabstat,price:user.price,buyername: req.body.buyername,approved:user.approved};
+  db.collection("propsell").updateOne(myquery, newvalues, function(err, res) {
+    if (err) throw err;
+    console.log("1 document updated");
+    db.close();
+  });
+  res.end("buyer is added");
+});
+  });
+ });
+
+
+
+app.post('/approveb',urlencodedParser,function(req,res){
+ 
+   MongoClient.connect(url2, function(err, db) {
+    db.collection('propsell').findOne({ landid: req.body.landidd}, function(err, user) {
+    
+              
+            //  if(user ===null)
+            //  {
+            //    res.end(" invalid landid");
+            // }
+            if (err) throw err;
+  var myquery = { approved: "false" };
+  var newvalues = { landid:user.landid,Aadhaarno:user.Aadhaarno,ownedaddress:user.ownedaddress,prevownname:user.prevownname,cordone:user.cordone,cordtwo:user.cordtwo,area:user.area,taxstat:user.taxstat,liabstat:user.liabstat,price:user.price,buyername: user.buyername,approved:'true'};
+  db.collection("propsell").updateOne(myquery, newvalues, function(err, res) {
+    if (err) throw err;
+    console.log("1 document updated");
+    db.close();
+  });
+  res.end("land is approved");
+});
+  });
+ });
+
+
+//insert into sell database==============================
+app.post('/gemo',urlencodedParser,function(req,res){
+   // fs.readFile('test.txt', 'utf8', function(err, data) {  
+   //  if (err) throw err;
+   //  console.log(data);
+   MongoClient.connect(urll, function(err, db) {
+   
+
+    db.collection('own_property').findOne({landid: req.body.landid},function(err, user) {
+           if (err) throw err;
+           var ob_data={'landid':user.landid,'Aadhaarno':user.Aadhaarno,'ownedaddress':user.ownedaddress,'prevownname':user.prevownname,'cordone':user.cordone,'cordtwo':user.cordtwo,'area':user.area,'taxstat':user.taxstat,'liabstat':user.liabstat,'price':user.price,'buyername': " ",'approved':'false'}
+           var obj1=JSON.stringify(ob_data);
+           res.send("your land is for sale now")
+     db.close();
+           
+         
+    console.log("Final reg Data : "+obj1);
+   var jsonObj = JSON.parse(obj1);
+      MongoClient.connect('mongodb://localhost:27017/sell', function(err, db) {
+      db.collection("propsell").insertOne(jsonObj, function(err, res) {
      if (err) throw err;
      console.log("1 document inserted");
      db.close();
+
       });
-       res.render('completeprofile',{profileData:req.body});
-      });
-    });
+});
+
+  });
+
+ });
+
+ });
+
+//register profile to MongoDB================================================================
+  app.post('/completeprofile',urlencodedParser,function(req,res){
+  MongoClient.connect(url3, function(err, db) {
+   
+
+    db.collection('profile1').findOne({adhar: req.body.aadhaar},function(err, user) {
+        if(user ===null){
+               res.end("register invalid");
+            }
+            else if (user.adhar === req.body.aadhaar)
+            {
+               var obj = JSON.stringify(req.body);
+               console.log("Final reg Data : "+obj);
+               var jsonObj = JSON.parse(obj);
+                  MongoClient.connect(url, function(err, db) {
+                  db.collection("userprofile").insertOne(jsonObj, function(err, res) {
+                 if (err) throw err;
+                 console.log("1 document inserted");
+                 db.close();
+                  });
+                   res.render('completeprofile',{profileData:req.body});
+                  });
+              
+          } 
+          else {
+            console.log("Credentials wrong");
+            res.end("registration invalid invalid");
+          }
+   });
+ });  
+}); 
 });
--- public/login.js
+++ public/login.js
@@ -1,5 +1,6 @@
+``
 $(document).ready(function(){
-   $("#regBtn").click(function(){
+   /*$("#regBtn").click(function(){
        $.ajax({
          type : 'GET',
          url : '/register',
@@ -8,34 +9,232 @@ $(document).ready(function(){
          }
        });
    });
+*/
 
-   $("#loginBtn").click(function(){
+ //paybutton  checking
+  $("#payform").click(function(){
+     var landidd  = $("#land").val();
+    
+     var cancelData ={'landidd':landidd};
        $.ajax({
-         type : 'GET',
-         url : '/login',
+         type : 'POST',
+         url : '/paycheck',
+         data:cancelData,
          success: function(data){
-           $("#loginDiv").html(data);
+           $("#payyDiv").html(data);
          }
        });
    });
-  $("#saleBtn").click(function(){
+
+//notary login=======================
+  $("#notloginBtn").click(function(){
+        $.ajax({
+          type : 'GET',
+          url : '/notlog',
+          success: function(data){
+            $("#notloginDiv").html(data);
+          }
+      });
+  
+    });
+  //pay price button=================
+    $("#opayBtn").click(function(){
        $.ajax({
          type : 'GET',
-         url : '/sales1',
+         url : '/opay',
+         success: function(data){
+           $("#opayDiv").html(data);
+         }
+       });
+   });
+//add buyer button==============
+  $("#addBtn").click(function(){
+       $.ajax({
+         type : 'GET',
+         url : '/buyer',
+         success: function(data){
+           $("#addDiv").html(data);
+         }
+       });
+   });
+
+    $("#pBtn").click(function(){
+       $.ajax({
+         type : 'GET',
+         url : '/pay',
+         success: function(data){
+           $("#payDiv").html(data);
+         }
+       });
+   });
+
+ // $("#publicBtn").click(function(){
+ //        $.ajax({
+ //          type : 'GET',
+ //          url : '/public',
+ //          success: function(data){
+ //            $("#publicDiv").html(data);
+ //          }
+ //      });
+  
+ //    });
+
+ $("#alrdBtn").click(function(){
+        $.ajax({
+          type : 'GET',
+          url : '/already',
+          success: function(data){
+            $("#viewDiv").html(data);
+          }
+      });
+  
+    });
+  $("#yetBtn").click(function(){
+        $.ajax({
+          type : 'GET',
+          url : '/yet',
+          success: function(data){
+            $("#publicDiv").html(data);
+          }
+      });
+  
+    });
+
+   $("#usrBtn").click(function(){
+        $.ajax({
+          type : 'GET',
+          url : '/userr',
+          success: function(data){
+            $("#userDiv").html(data);
+          }
+      });
+  
+    });
+   $("#notBtn").click(function(){
+        $.ajax({
+          type : 'GET',
+          url : '/nota',
+          success: function(data){
+            $("#notaDiv").html(data);
+          }
+      });
+  
+    });
+   $("#loginBtn").click(function(){
+        $.ajax({
+          type : 'GET',
+          url : '/login',
+          success: function(data){
+            $("#loginDiv").html(data);
+          }
+      });
+   //window.location="login"
+    });
+
+  $("#sellBtn").click(function(){
+     var landid  = $("#landid").val();
+    
+     var sellData ={'landid':landid};
+       $.ajax({
+         type : 'POST',
+         url : '/gemo',
+        data:  sellData,
          success: function(data){
            $("#saleDiv").html(data);
          }
        });
    });
+  //cancel button checking
+  $("#cancelBtn").click(function(){
+     var landidd  = $("#landidd").val();
+    
+     var cancelData ={'landidd':landidd};
+       $.ajax({
+         type : 'POST',
+         url : '/cancel',
+         data:cancelData,
+         success: function(data){
+           $("#cancelDiv").html(data);
+         }
+       });
+   });
+
+    //cancel button checking
+  $("#yetaddform").click(function(){
+     var landidd  = $("#aname").val();
+    
+     var cancelData ={'landidd':landidd};
+       $.ajax({
+         type : 'POST',
+         url : '/approveb',
+         data:cancelData,
+         success: function(data){
+           $("#yetaddDiv").html(data);
+         }
+       });
+   });
+
+
+  //add button 
+  $("#addform").click(function(){
+     var buyername =$('#bname').val();
+     var landidd  = $("#bid").val();
+   
+     var addData ={'buyername':buyername,'landidd':landidd};
+       $.ajax({
+         type : 'POST',
+         url : '/addb',
+         data: addData,
+         success: function(data){
+           $("#addDiv").html(data);
+         }
+       });
+   });
+
+  $("#viewBtn").click(function(){
+       $.ajax({
+         type : 'GET',
+         url : '/view1',
+         success: function(data){
+           $("#viewDiv").html(data);
+         }
+       });
+   });
+    //==============change price===================================
+    $("#changepriceBtn").click(function(){
+       $.ajax({
+         type : 'GET',
+         url : '/changep',
+         success: function(data){
+         $("#changeprice").html(data);
+         }
+       });
+   });
+
+  //===changeform==================================
+  $("#changeForm").click(function(){
+     var price  = $("#price").val();
+     var landid  = $("#land").val();
+     var viewData ={'price':price,'landid':landid};
+     $.ajax({
+         type : 'POST',
+         url : '/changeprice',
+         data : viewData,
+         success: function(data){
+         $("#changeprice").html(data);
+         }
+       });
+
+   });
        //=====Sales Form Request=============================================
-   $("#saleForm").click(function(){
-     var uname  = $("#uname").val();
-     var land_id = $("#land_id").val();
-     var saleData ={'name': uname,'Land_id': land_id};
+   $("#viewForm").click(function(){
+     var aadhaar  = $("#aadhaar").val();
+    
+     var viewData ={'Aadhaarno':aadhaar};
      $.ajax({
          type : 'POST',
          url : '/demo1',
-         data : saleData,
+         data : viewData,
          success: function(data){
          $("#mainDiv").html(data);
          }
@@ -57,17 +256,29 @@ $(document).ready(function(){
        });
 
    });
-//=====Register Form=============================================
-   $("#regForm").click(function(){
-     var uname  = $("#uname").val();
-     var upass = $("#upass").val();
-     var regData ={'name': uname,'pass':upass};
-       $.ajax({
+   //========Notary Login Form request=================
+   $("#notloginForm").click(function(){
+     var uuname  = $("#unname").val();
+     var uupass = $("#unpass").val();
+     var notloginData ={'name': uuname,'pass':uupass};
+     $.ajax({
          type : 'POST',
-         url : '/regiterToDb',
-         data : regData,
+         url : '/notaryy',
+         data : notloginData,
          success: function(data){
-         $("#mainDiv").html(data);
+         $("#notloginDiv").html(data);
+         }
+       });
+
+   });
+ //Register Form=============================================
+   $("#regBtn").click(function(){
+       $.ajax({
+         type : 'GET',
+         url : '/regiterToDb',
+         /*data : regData,*/
+         success: function(data){
+         $("#regDiv").html(data);
          }
        });
    });
@@ -76,11 +287,8 @@ $('#saveBtn').click(function(){
   var email = $("#email").val();
   var phone = $("#phone").val();
   var aadhaar = $("#aadhaar").val();
-
-
-  var name = $("#name").val();
-  var pass = $("#pass").val();
-  var profileData = {'email':email,'phone':phone,'aadhaar':aadhaar,'name' : name,'pass' : pass};
+  var pass = $("#upass").val();
+  var profileData = {'email':email,'phone':phone,'aadhaar':aadhaar,'pass' : pass};
   $.ajax({
     type : 'POST',
     url : '/completeprofile',
@@ -91,3 +299,5 @@ $('#saveBtn').click(function(){
   });
 });
 });
+
+
